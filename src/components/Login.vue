9cf3a83783a29882c990415bd579e05905095996
white <white11013@163.com>
2017-10-03 11:27:57 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container grid-list-xl text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs10 offset-xs1>
+        <v-form v-model="valid">
+          <v-text-field label="Name" v-model="name" :rules="nameRules" :counter="10" required></v-text-field>
+          <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to login page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+.main
+  width: 450px;
+  background: grey;
+</style>
